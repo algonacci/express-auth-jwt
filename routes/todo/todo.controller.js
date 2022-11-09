0bdf507af993aa244d39e681d8e797d89594840c
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2022-11-09 12:47:21 +0700

    Update todo.controller.js

--- routes/todo/todo.controller.js
+++ routes/todo/todo.controller.js
@@ -1,16 +1,10 @@
 const todo = (req, res) => {
-  fetch("https://jsonplaceholder.typicode.com/todos/1")
+  fetch("https://jsonplaceholder.typicode.com/todos")
     .then((data) => data.json())
     .then((data) => {
-      const todos = {
-        userId: data.userId,
-        id: data.id,
-        title: data.title,
-        completed: data.completed,
-      };
       return res.status(200).json({
         statusCode: 200,
-        data: todos,
+        data: data,
       });
     });
 };
